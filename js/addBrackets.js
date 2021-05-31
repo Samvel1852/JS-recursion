bf353387ddd144d70ede58112e5bc6870435e215
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-05-31 18:30:02 +0400

    feat: Added addBrackets function

---
+++ js/addBrackets.js
@@ -0,0 +1,26 @@
+let str1 = "asdfghjk";
+let str2 = "asdfgds";
+
+function addBrackets(str, res = "", i = 0) {
+  if (i === str.length) {
+    return res;
+  }
+
+  let center = Math.floor(str.length / 2);
+
+  if (i < center) {
+    res += "(" + str[i];
+  }
+
+  if (i === center) {
+    res += str.length % 2 === 0 ? str[i] + ")" : "(" + str[i] + ")";
+  }
+
+  if (i > center) {
+    res += str[i] + ")";
+  }
+
+  return addBrackets(str, res, (i += 1));
+}
+
+console.log(addBrackets("Y"));
